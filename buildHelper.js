462cfc2fcb161b441947e30dddf281ef018ed1b0
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-09-28 01:53:13 +0530

    Test build helper

--- buildHelper.js
+++ buildHelper.js
@@ -10,6 +10,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const execAsync = promisify(exec);
 
+/**
+ * Get repo details, repoUrl, ghPageURL (general prediction)
+ *
+ * reusable
+ */
 export default async function getRepoDetails() {
   const { stdout: gitConfig } = await execAsync("cat .git/config");
 
@@ -42,6 +47,11 @@ export default async function getRepoDetails() {
     viteBaseName: repoName || __dirname.split(path.sep).at(-1), // get from repo, or fallback to folder name
   };
 }
+getRepoDetails.tests = [
+  async () => {
+    console.log(await getRepoDetails());
+  },
+];
 
 /**
  * Set package.json
@@ -52,6 +62,8 @@ export default async function getRepoDetails() {
  * Note: the callback param is a cached package.json (it will remain same irrespective of number of calls made)
  *
  * @returns {void}
+ *
+ * note: reusable function with the import - `import PACKAGE_JSON from "./package.json" assert { type: "json" };`
  */
 export const setPackageJSON = async (
   valueOrFunction = (originalValue) => {
@@ -67,15 +79,40 @@ export const setPackageJSON = async (
     JSON.stringify(newValue)
   );
 };
+setPackageJSON.tests = [
+  async () => {
+    // homepage changes upon running
+    await setPackageJSON({ homepage: "example.com" });
+  },
+  async () => {
+    // there should be no change.
+    await setPackageJSON({ homepage: "example.com" });
+    await setPackageJSON(); // default is to reset from cached
+  },
+];
 
-// console.log(await getRepoDetails());
+/**
+ * Relevant to current set up only.
+ *
+ * So we want to get github pages URL and set it in package.json
+ * Use the `getRepoDetails` function
+ */
+export const setHomePageInPackageJSON = async (value = null) => {
+  const { ghURL = "" } = await getRepoDetails();
+  return setPackageJSON((currentValue) => ({
+    ...currentValue,
+    homepage: value || ghURL,
+  }));
+};
+setHomePageInPackageJSON.tests = [
+  async () => {
+    // homepage becomes 'example.com'
+    await setHomePageInPackageJSON("example.com");
+  },
+];
 
-// Set base in vite.config.js
-// Set homepage in package.json
-
-// console.log(await getRepoDetails());
-// await setPackageJSON((currentValue) => ({
-//   ...currentValue,
-//   homepage: "example.com",
-// }));
-// await setPackageJSON((currentValue) => ({ ...currentValue }));
+// Goal:
+// 1. Set base in vite.config.js
+// 2. Set homepage in package.json
+// 3. Run the build commands
+// 4. Try to reset everything (`git checkout `), but GitHUb clones the repo again for each deploy, so may not be needed.
