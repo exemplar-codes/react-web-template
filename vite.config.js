17a56e12f401deb5d7c2ddb2e3e06a4b3b0bf2cd
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2024-08-13 14:27:30 +0530

    Update linters

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "@svgr/rollup";
+import eslint from "vite-plugin-eslint";
 
 import {
   getRepoDetails,
@@ -14,12 +16,31 @@ if (false && import.meta.env?.PROD) {
   await setPackageJSONValuesForCurrentRepo(repoName, ghURL); // set package.json homepage for gh pages
 }
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({ dimensions: true, svgo: false, typescript: false }),
-  ],
-  base: `/${viteBaseName}/`,
-  assetsInclude: ["**/*.zip"], // for zip and iframe assets
-});
+export default ({ mode }) => {
+  // using function notation so loaded envs can be used
+  const loadedEnvs = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [
+      eslint(),
+      react(),
+      svgr({ dimensions: true, svgo: false, typescript: false }),
+    ],
+    base: `/${viteBaseName}/`,
+    assetsInclude: ["**/*.zip"], // for zip and iframe assets
+    resolve: {
+      alias: {
+        "@": resolve(import.meta.dirname, "src"),
+        /*
+    Aliased imports (preferred):
+    - Alias for 'src': import MyComponent from "@/components/MyComponent"
+
+    Other imports patterns till work:
+    - npm imports: import React from 'react'
+    - Org npm imports: "@redux/toolkit". "@/" doesnt clash since immediate slash.
+    - Relative imports: "./components/MyComponent"
+    */
+      },
+    },
+  });
+};
